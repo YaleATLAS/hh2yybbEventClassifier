1f988f6c204d45b37f39532ef2578884ee0136ec
Ubuntu <ubuntu@ip-172-31-29-106.eu-central-1.compute.internal>
2016-06-22 13:19:29 +0000

    convert script to function, extract w, condense xjet xmuons xphotons to one function

--- readinformat.py
+++ readinformat.py
@@ -1,10 +1,6 @@
-'''Given files with data
+'''Given files with data (in this example: ttbar, qcd, and wjets)
    Concatenates all events
-   Creates (number of events)x(number of related branches) np array
-   Under the names of:
-   jet_branches
-   muon_branches
-   photon_branches
+   Creates Xjets, Xmuons, Xphotons, w, and y   
 
 '''
 
@@ -14,8 +10,8 @@ from numpy.lib.recfunctions import stack_arrays
 from root_numpy import root2array
 import pandas as pd
 import glob
+import argparse
 
-#convert root files to pandas
 def root2pandas(files_path, tree_name, **kwargs):
 	files = glob.glob(files_path)
 	ss = stack_arrays([root2array(fpath, tree_name, **kwargs).view(np.recarray) for fpath in files])
@@ -24,35 +20,42 @@ def root2pandas(files_path, tree_name, **kwargs):
     	except Exception:
         	return pd.DataFrame(ss.data)
 
-ttbar = root2pandas('/home/ubuntu/jenny/ttbar.root', 'events')
-qcd = root2pandas('/home/ubuntu/jenny/qcd.root', 'events')
-wjets = root2pandas('/home/ubuntu/jenny/wjets.root', 'events')
+def x(events, phrase):
+        return events[[key for key in events.keys() if phrase in key]]
 
+def readin(ttbar_path, qcd_path, wjets_path):
+	#convert root files to pandas
+	ttbar = root2pandas(ttbar_path, 'events')
+	qcd = root2pandas(qcd_path, 'events')
+	wjets = root2pandas(wjets_path, 'events')
+	
+	
+	#create column indicating background(0)/signal(1)
+	ttbar['y'] = 1
+	qcd['y'] = 0
+	wjets['y'] = 0
+	
+	
+	#join the arrays together
+	all_events = pd.concat([ttbar, qcd, wjets], ignore_index=True)
+	
+	
+	#slice related branches
+	all_events_jets = x(all_events, 'Jet')
+	all_events_muons = x(all_events, 'Muon')
+	all_events_photons = x(all_events, 'Photon')
+	all_events_weights = all_events['EventWeight']
+	all_events_y = all_events['y']
+	
+	
+	#convert to np arrays
+	jet_branches = all_events_jets.as_matrix()
+	muon_branches = all_events_muons.as_matrix()
+	photon_branches = all_events_jets.as_matrix()
+	w_branch = all_events_weights.values
+	y_branch = all_events_y.values
+	
+	return jet_branches, muon_branches, photon_branches, w_branch, y_branch
 
-#create column indicating background(0)/signal(1)
-ttbar['y'] = 1
-qcd['y'] = 0
-wjets['y'] = 0
-
-
-#join the arrays together
-all_events = pd.concat([ttbar, qcd, wjets], ignore_index=True)
-
-
-#slice related branches
-def xjets(events):
-	return events[[key for key in events.keys() if 'Jet' in key or key=='y']]
-def xmuons(events):
-	return events[[key for key in events.keys() if 'Muon' in key or key=='y']]
-def xphotons(events):
-        return events[[key for key in events.keys() if 'Photon' in key or key=='y']]
-
-all_events_jets = xjets(all_events)
-all_events_muons = xmuons(all_events)
-all_events_photons = xphotons(all_events)
-
-
-#convert to np arrays
-jet_branches = all_events_jets.as_matrix()
-muon_branches = all_events_muons.as_matrix()
-photon_branches = all_events_jets.as_matrix()
+#test
+readin('/home/ubuntu/jenny/ttbar.root', '/home/ubuntu/jenny/qcd.root','/home/ubuntu/jenny/ttbar.root')
