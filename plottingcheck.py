7fd3b5d70561b799a0472450f46b4a94c14b55b4
Ubuntu <ubuntu@ip-172-31-29-106.eu-central-1.compute.internal>
2016-06-29 14:49:01 +0000

    fix bugs

--- plottingcheck.py
+++ plottingcheck.py
@@ -2,61 +2,52 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm 
-
-def flatten(column):
-    '''
-    Args:
-    -----
-        column: a column of a pandas df whose entries are lists (or regular entries -- in which case nothing is done)
-                e.g.: my_df['some_variable'] 
-
-    Returns:
-    --------    
-        flattened out version of the column. 
-
-        For example, it will turn:
-        [1791, 2719, 1891]
-        [1717, 1, 0, 171, 9181, 537, 12]
-        [82, 11]
-        ...
-        into:
-        1791, 2719, 1891, 1717, 1, 0, 171, 9181, 537, 12, 82, 11, ...
-    '''
-    try:
-        return np.array([v for e in column for v in e])
-    except (TypeError, ValueError):
-        return column
+import pandautils as pup       
 
 def _plot_X(train, test, y_train, y_test, w_train, w_test, varlist, feature):
-	'''
-	Args:
+	'''Args:
 		train:
 		test:
 		feature: a string like 'Jet', 'Muon', 'Photon'
 	'''
-	color=iter(cm.rainbow(np.linspace(0,1,len(varlist)*2)))
 
-	for i, key in enumerate(varlist):
+	w_train_ext = np.array(pup.flatten([[w]*(len(train[i,0])) for i, w in enumerate(w_train)]))
+	w_test_ext = np.array(pup.flatten([[w]*(len(test[i,0])) for i, w in enumerate(w_test)]))
+	y_train_ext = np.array(pup.flatten([[y]*(len(train[i,0])) for i, y in enumerate(y_train)]))
+	y_test_ext = np.array(pup.flatten([[y]*(len(test[i,0])) for i, y in enumerate(y_test)]))
+
+	column_counter=0
+	for key in varlist:
 		if key.startswith(feature):
+			flat_train = pup.flatten(train[:,column_counter])
+			flat_test = pup.flatten(test[:,column_counter])
 			matplotlib.rcParams.update({'font.size': 16})
 			fig = plt.figure(figsize=(11.69, 8.27), dpi=100)
-			bins = np.linspace(min(min(flatten(train[:,i])), min(flatten(test[:,i]))), 
-				max(max(flatten(train[:,i])), max(flatten(test[:,i]))), 30)
-			print bins
+			bins = np.linspace(min(min(pup.flatten(train[:,column_counter])), 
+				min(pup.flatten(test[:,column_counter]))), 
+				max(max(pup.flatten(train[:,column_counter])), 
+				max(pup.flatten(test[:,column_counter]))), 30)
+			column_counter=column_counter+1
+			color=iter(cm.rainbow(np.linspace(0,1,2)))
 			for k in range(len(np.unique(y_train))):
 				c=next(color)
-				_ = plt.hist(train[y_train==k,i], bins=bins, histtype='stepfilled', 
-					label='Xtr', weights=w_train[y_train==k], linewidth=2, color = c, alpha=0.5)
-				_ = plt.hist(test[y_test==k,i], bins=bins, histtype='step', 
-					label='Xte', weights=w_test[y_test==k], linewidth=2, color = c, alpha=0.5)
+				_ = plt.hist(flat_train[y_train_ext==k], bins=bins, histtype='step', normed=True, 
+					label='Xtr class:'+str(k),weights=w_train_ext[y_train_ext==k],
+					color = c, linewidth=1)
+				_ = plt.hist(flat_test[y_test_ext==k], bins=bins, histtype='step', normed=True,
+					label='Xte class:'+str(k),weights=w_test_ext[y_test_ext==k], 
+					linewidth=2, color = c, linestyle='dashed')	
 			plt.xlabel(key)
+			plt.yscale('log')
 			plt.ylabel('Weighted Events')
 			plt.legend()
+			plt.plot()
 			plt.show()
 			plt.savefig(key+'.pdf')
 
 def plot_inputs(X_jets_train, X_jets_test, X_photons_train, X_photons_test, 
 	X_muons_train, X_muons_test, y_train, y_test, w_train, w_test, varlist):
+	
 	_plot_X(X_jets_train, X_jets_test, y_train, y_test, w_train, w_test, varlist, 'Jet')
 	_plot_X(X_photons_train, X_photons_test, y_train, y_test, w_train, w_test, varlist, 'Photon')
 	_plot_X(X_muons_train, X_muons_test, y_train, y_test, w_train, w_test, varlist, 'Muon')
