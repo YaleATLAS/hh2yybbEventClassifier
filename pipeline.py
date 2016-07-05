da3a839af487719431019ce621a76181315a15b7
Gigi Stark <gigifstark@Gigis-MacBook-Pro.local>
2016-07-05 18:00:51 +0200

    Simple Neural Network with Plotting and Separate Train and Test Functions

--- pipeline.py
+++ pipeline.py
@@ -2,9 +2,11 @@ import json
 from data_processing import read_in, shuffle_split_scale, zero_padding
 import pandautils as pup
 import cPickle
-from plotting import plot_inputs
+from plotting import plot_inputs, plot_NN
 import utils
 import logging
+from neural_network import NN_train, NN_test
+import deepdish.io as io
 #from plotting import plot_inputs, plot_performance
 #from nn_model import train, test
 
@@ -99,7 +101,7 @@ def main(json_config, exclude_vars):
     This should produce normed, weighted histograms of the input distributions for all variables
     The train and test distributions should be shown for every class
     Plots should be saved out a pdf with informative names
-    '''
+    
     logger.info('Plotting input distributions')
     plot_inputs(
         X_jets_train, X_jets_test, 
@@ -109,7 +111,7 @@ def main(json_config, exclude_vars):
         w_train, w_test,
         varlist 
         )
-
+    '''
     X_jets_train, X_jets_test, \
     X_photons_train, X_photons_test, \
     X_muons_train, X_muons_test = map(zero_padding, 
@@ -121,10 +123,19 @@ def main(json_config, exclude_vars):
         [5, 5, 3, 3, 2, 2]
     )
 
-    print  X_jets_train.shape, X_photons_train.shape
-
     # # -- train
     # # design a Keras NN with three RNN streams (jets, photons, muons)
+    io.save(('X_jets_NN.h5'), NN(X_jets_train, X_jets_test, y_train))
+    X_jets_NN_h5 = io.load('X_jets_NN.h5')
+    io.save(('X_photons_NN.h5'), NN(X_photons_train, X_photons_test, y_train))
+    X_photons_NN_h5 = io.load('X_photons_NN.h5')
+    io.save(('X_muons_NN.h5'), NN(X_muons_train, X_muons_test, y_train))
+    X_muons_NN_h5 = io.load('X_muons_NN.h5')
+  
+    plot_NN(NN_test(X_jets_test, NN_train(X_jets_train, y_train)), y_test, w_test)
+    plot_NN(NN_test(X_photons_test, NN_train(X_photons_train, y_train)), y_test, w_test)
+    plot_NN(NN_test(X_muons_test, NN_train(X_muons_train, y_train)), y_test, w_test)
+
     # # combine the outputs and process them through a bunch of FF layers
     # # use a validation split of 20%
     # # save out the weights to hdf5 and the model to yaml
